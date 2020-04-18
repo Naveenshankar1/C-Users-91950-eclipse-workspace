9799bb2a6cf9f8706babc5b67cd366e9d230f002
Naveenshankar1 <navaneethashankar36@gmail.com>
2020-04-18 15:05:49 +0530

    Change Saved

--- Overloading/src/Bank/Bank1.java
+++ Overloading/src/Bank/Bank1.java
@@ -6,7 +6,7 @@ public class Bank1 extends Ticketnew {
 
 	public static void main(String[] args) {
 		Bank1 obj =new Bank1();
-		obj.add(20, 30.80);
-		obj.add(10, 20.80);
+		obj.add(200, 30.80);
+		obj.add(100, 20.80);
 	}
 }
